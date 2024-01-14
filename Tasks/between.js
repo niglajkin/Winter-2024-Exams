238e3580a2fb36a5b61eeb13810ff09932ebbe2d
Ihlaikin Mykola <143129114+niglajkin@users.noreply.github.com>
2024-01-14 21:50:43 +0200

    Update between.js

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,27 +1,18 @@
 // Extract substring between prefix and suffix
 
-//Step 1
-//Add 'use strict'
-//Change naming
-//Define variables
+//Step 2
+//Remove unneeded blocks
 
-'use strict';
+'use strict'
 
 const getValueBetween = (searchedString, prefix, suffix) => {
   let index = searchedString.indexOf(prefix);
-  if (index === -1) return '';
-  else {
-    const indexWithoutPrefix = index + prefix.length;
-    searchedString = searchedString.substring(indexWithoutPrefix);
-    if (searchedString) {
-      index = searchedString.indexOf(suffix);
-      if (index === -1) {
-        return '';
-      } else {
-        searchedString = searchedString.substring(0, index);
-      }
-    }
-  }
+  if (index === -1) return 'incorrect input';
+  const indexWithoutPrefix = index + prefix.length;
+  searchedString = searchedString.substring(indexWithoutPrefix);
+  index = searchedString.indexOf(suffix);
+  if (index === -1) return 'incorrect input';
+  searchedString = searchedString.substring(0, index);
   return searchedString;
 };
 
