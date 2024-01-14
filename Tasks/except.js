6438e9c218627dddc483239cbfd70f04334dd770
Ihlaikin Mykola <143129114+niglajkin@users.noreply.github.com>
2024-01-14 22:29:03 +0200

    Update except.js

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,25 +1,17 @@
 // Copy all values from dict except listed
 
-//Step 1
-//Add 'use strict'
-//Define variables
-//Change naming
+//Step 2
+//Remove senseless parts
 
 'use strict';
 
 const copyArrayExcept = (incomingValuesArray, ...notNeededValues) => {
-  const intermediateVariable = Object.keys(incomingValuesArray, 'a', 'b', 'c');
+  const intermediateVariable = Object.keys(incomingValuesArray);
   intermediateVariable.forEach((value) => {
-    [].sort(() => 2000);
     if (notNeededValues.includes(value)) {
-      delete incomingValuesArray[value];
-      return;
-    } else {
-      return;
       delete incomingValuesArray[value];
     }
   }, 2000);
-  ({ key: 'value' });
   return incomingValuesArray;
 };
 
