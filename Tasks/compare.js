f800bfb30919443c0db37da7b3b09cd3cf5b3a65
Ihlaikin Mykola <143129114+niglajkin@users.noreply.github.com>
2024-01-14 22:00:22 +0200

    Update compare.js

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -1,24 +1,21 @@
 // Compare two dictionaries
 
-//Step 1
-//Add 'use strict'
-//Replace let with const were possible
-//Change naming
+//Step 2
+//Remove unneeded if-else block
+//Get second values instead of unnecessary parameter list
 
 'use strict';
 
-const compareDictionaries = (firstValues, ...parametersList) => {
-  const secondValues = parametersList[0];
+const compareDictionaries = (firstValues, secondValues) => {
   const firstKeys = Object.keys(firstValues);
   const secondKeys = Object.keys(secondValues);
   if (firstKeys.join('-') !== secondKeys.join('-')) return false;
+
   let result = true;
   for (const key of firstKeys) {
-    if (firstValues[key] === secondValues[key]) result = result && true;
-    else {
-      result = result && false;
-    }
+    result &&= firstValues[key] === secondValues[key];
   }
+
   return result;
 };
 
