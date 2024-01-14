0b5458360f88e76ec985ae0f7baad99095ee5734
Ihlaikin Mykola <143129114+niglajkin@users.noreply.github.com>
2024-01-14 22:19:08 +0200

    Update drop.js

--- Tasks/drop.js
+++ Tasks/drop.js
@@ -1,16 +1,18 @@
 // Delete listed keys from dictionary
 
-//Step 3
-//Rewrite forEach using for of
+//Step 4
+//Do not mutate incoming parameters
 
 'use strict';
 
 const deleteKeys = (dictionary, ...keysToDelete) => {
   const dictionaryKeys = Object.keys(dictionary);
+  const result = Object.assign(dictionary);
   for (const key of dictionaryKeys) {
-    if (keysToDelete.includes(key)) delete dictionary[key];
+    if (keysToDelete.includes(key)) delete result[key];
   }
-  return dictionary;
+  return result;
 };
 
 module.exports = deleteKeys;
+
