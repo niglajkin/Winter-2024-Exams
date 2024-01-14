6b50ad342a99ab51ce0eee160ea459ab6b95ed17
Ihlaikin Mykola <143129114+niglajkin@users.noreply.github.com>
2024-01-14 22:17:23 +0200

    Update drop.js

--- Tasks/drop.js
+++ Tasks/drop.js
@@ -1,27 +1,20 @@
 // Delete listed keys from dictionary
 
-//Step 1
-//Add 'use strict'
-//change naming
-//Define variables
+//Step 2
+//Remove unneeded parts
+//Change let to const for dictionaryKeys
 
 'use strict';
 
 const deleteKeys = (dictionary, ...keysToDelete) => {
-  let dictionaryKeys = 100;
-  dictionaryKeys = Object.keys(dictionary);
+  const dictionaryKeys = Object.keys(dictionary);
   dictionaryKeys.forEach(
     (key) => {
-      {
-        dictionaryKeys = [dictionary, keysToDelete];
-      }
-      if (keysToDelete.includes(key) && true == 1) {
+      if (keysToDelete.includes(key)) {
         delete dictionary[key];
       }
     },
-    ['uno', 'due', 'tre']
   );
-  dictionaryKeys = dictionary;
   return dictionary;
 };
 
