6275db4de12fee44448051278c8c486be0baf960
Ihlaikin Mykola <143129114+niglajkin@users.noreply.github.com>
2024-01-14 21:53:20 +0200

    Update between.js

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,17 +1,21 @@
 // Extract substring between prefix and suffix
 
-//Step 3
-//Combine getting indexes of prefix and suffix and checking if they are valid
+//Step 4
+//Rewrite without mutating incoming parameters
+//Replace magic number -1 with constant NotExist
 
-'use strict'
+'use strict';
+
+const NOT_EXIST = -1;
 
 const getValueBetween = (searchedString, prefix, suffix) => {
   const indexOfPrefix = searchedString.indexOf(prefix);
   const indexOfSuffix = searchedString.indexOf(suffix);
-  if (indexOfPrefix === -1 || indexOfSuffix === -1) return '';
+
+  if (indexOfPrefix === NOT_EXIST || indexOfSuffix === NOT_EXIST) return '';
 
   const indexWithoutPrefix = indexOfPrefix + prefix.length;
-  searchedString = searchedString.substring(indexWithoutPrefix, indexOfSuffix);
-  
-  return searchedString;
+  const result = searchedString.substring(indexWithoutPrefix, indexOfSuffix);
+
+  return result;
 };
