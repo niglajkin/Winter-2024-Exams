ea41bcdfe4f8a38844b8d97dbab3ee962f2923d2
Ihlaikin Mykola <143129114+niglajkin@users.noreply.github.com>
2024-01-14 22:12:21 +0200

    Update distinct.js

--- Tasks/distinct.js
+++ Tasks/distinct.js
@@ -1,23 +1,21 @@
 // Return an array without duplicates
 
-//Step 1
-//Add 'use strict';
-//Define variables
-//Change naming
+//Step 2
+//Rewrite using for of instead of forEach
 
 'use strict';
 
 const removeDuplicates = (data) => {
   const result = new Set();
   let index = 0;
-  data.forEach((element) => {
+  for (const element of data) {
     if (result.has(element)) {
       delete data[index];
     } else {
       result.add(element);
     }
     index++;
-  });
+  }
   return data.filter((x) => typeof x === 'number');
 };
 
