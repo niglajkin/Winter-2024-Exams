388ca9c5794da7afd8de0cc0e2dd10c8ed858fd6
Ihlaikin Mykola <143129114+niglajkin@users.noreply.github.com>
2024-01-14 22:25:43 +0200

    Update duplicate.js

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,21 +1,17 @@
 // Return an array without duplicates
 
-//Step 1
-//Define variables
-//Add use strict
-//Change naming
-
+//Step 2
+//Remove unneeded else block
 'use strict';
 
 const duplicateElements = (value, arrayLength) => {
   if (arrayLength <= 0) return [];
-  else {
-    const arrayWithDuplicates = [];
-    for (let i = 0; i < arrayLength; i++) {
-      arrayWithDuplicates[i] = value;
-    }
-    return arrayWithDuplicates;
+
+  const arrayWithDuplicates = [];
+  for (let i = 0; i < arrayLength; i++) {
+    arrayWithDuplicates[i] = value;
   }
+  return arrayWithDuplicates;
 };
 
 module.exports = duplicateElements;
