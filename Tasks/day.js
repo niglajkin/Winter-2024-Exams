77f52081f7c57ec1cedfb188a29dd88eea6fdcfa
Ihlaikin Mykola <143129114+niglajkin@users.noreply.github.com>
2024-01-14 22:08:35 +0200

    Update day.js

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,17 +1,18 @@
 // Get day number
 
-//Step 2
-//Define DAYS with const instead of let
-//Remove unneeded {} after if
-
+//Step 3
+//Add incorrect input constant for -1;
 'use strict';
+
 const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const incorrectInput = -1;
 
 const getDayNumber = (day) => {
   for (let i = 0; i < DAYS.length; i++) {
     if (day.startsWith(DAYS[i].toLowerCase())) return i + 1;
   }
-  return -1;
+
+  return incorrectInput;
 };
 
 module.exports = getDayNumber;
