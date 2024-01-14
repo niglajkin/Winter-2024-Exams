ef06e4dc69fb02525fc2579a1d9d4cd061c2d1e9
Ihlaikin Mykola <143129114+niglajkin@users.noreply.github.com>
2024-01-14 22:01:35 +0200

    Update count.js

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,16 +1,21 @@
 // Sum all number values in dict
 
-count = (obj) => {
-  sum = 0;
-  {
-  }
-  keys = Object.keys(obj);
+//Step 1
+//Add 'use strict'
+//Remove unneeded blocks
+//Define variables
+
+'use strict';
+
+const count = (obj) => {
+  let sum = 0;
+  const keys = Object.keys(obj);
+
   keys.forEach((key) => {
-    [];
     const value = obj[key];
     if (typeof value === 'number') sum += value;
   });
-  ({});
+
   return sum;
 };
 
